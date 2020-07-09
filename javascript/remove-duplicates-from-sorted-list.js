6dd5170d0bd00ea8b9b3c44fbb92940cd19c7bc9
İsmail Hakkı Tekin <ihakkitekin@gmail.com>
2020-07-10 01:00:00 +0300

    Add remove-duplicates-from-sorted-list solution in JS

---
+++ javascript/remove-duplicates-from-sorted-list.js
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) { 
+  if(head == null || head.next == null) return head;
+ 
+  let current = head;
+
+  while(current.next) {
+    if(current.val == current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;   
+    }
+  }
+
+  return head;
+};
