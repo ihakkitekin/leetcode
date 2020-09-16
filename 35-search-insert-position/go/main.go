9359684b6763b385833c4c9282350ed6032bd87e
İsmail Hakkı TEKİN <ihakkitekin@gmail.com>
2020-09-16 22:07:56 +0300

    Add search-insert-position in go

---
+++ 35-search-insert-position/go/main.go
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/search-insert-position
+
+
+func searchInsert(nums []int, target int) int {
+		leng := len(nums)
+
+    for i:= 0; i < leng; i++ {
+			current := nums[i]
+			next := -1
+
+			if i != leng-1 {
+				next = nums[i + 1]
+			}
+
+			if current == target {
+				return i
+			}
+
+			if target > current && target < next {
+				return i + 1
+			}
+
+			if target < current {
+				return i
+			}
+		}
+
+		return len(nums)
+}
--- 35-search-insert-position/js/index.js
+++ 35-search-insert-position/js/index.js
@@ -19,9 +19,3 @@ var searchInsert = function (nums, target) {
 
   return nums.length;
 };
-
-console.log(searchInsert([1, 3, 5, 6], 5))
-console.log(searchInsert([1, 3, 5, 6], 2))
-console.log(searchInsert([1, 3, 5, 6], 7))
-console.log(searchInsert([1, 3, 5, 6], 0))
-console.log(searchInsert([1], 0))
