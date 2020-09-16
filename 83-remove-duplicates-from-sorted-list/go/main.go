8ace6fddda64f1b32c3578af3afec11a8790d952
İsmail Hakkı TEKİN <ihakkitekin@gmail.com>
2020-09-16 22:22:15 +0300

    Add remove-duplicates-from-sorted-list in go

---
+++ 83-remove-duplicates-from-sorted-list/go/main.go
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list
+
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if(head == nil || head.Next == nil) {
+		return head
+	}
+		
+	current	:= head
+
+	for head.Next != nil && current.Next != nil {
+		if current.Val == current.Next.Val {
+			current.Next = current.Next.Next
+		} else {
+			current = current.Next
+		}
+	}
+
+	return head
+}
