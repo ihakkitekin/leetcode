ccc1ac1419e88110d19e28cfe7975a9c53b80e32
İsmail Hakkı TEKİN <ihakkitekin@gmail.com>
2020-09-20 21:16:24 +0300

    Add climbing-stairs in go

---
+++ 70-climbing-stairs/go/main.go
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/climbing-stairs
+
+func climbStairs(n int) int {
+		m := make(map[int]int)
+
+    return calculate(n, m)
+}
+
+func calculate(n int, m map[int]int) int {
+	if n < 2 {
+		return 1
+	}
+
+	i, ok := m[n]
+
+	if ok {
+		return i
+	}
+
+	left := calculate(n - 1, m)
+	right := calculate(n - 2, m)
+
+	m[n] = left + right
+
+	return m[n]
+}
